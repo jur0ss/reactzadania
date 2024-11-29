ba8d7f3c5bd166d2be36ec32c89c094b66e9d675
jurekg1 <jurek@poczta.pl>
2024-11-29 09:00:56 +0100

    Added getAverage method using ...rest operator

--- oceny/oceny.js
+++ oceny/oceny.js
@@ -1,11 +1,22 @@
 class Student
 {
-    constructor(name,age,grades)
+    constructor(age, name ,...grades)
     {
         this.name = name
         this.age = age
-        this.grades = []
+        this.grades = grades
+    }
+    getAverage()
+    {
+        let sum = 0
+        for (let i=0;i<this.grades.length;i++)
+        {
+            sum+=this.grades[i]
+        }
+        let average = (sum/this.grades.length)
+        return average
     }
 }
 
-const grades1 = new Student("Jurek", 17, [2,3,5,4,6])
+const grades1 = new Student(17, "Jurek", 5, 6, 4, 3)
+console.log(grades1.getAverage())
