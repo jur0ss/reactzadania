916432a372d5dba37e686d093311623f7128e252
jurekg1 <jurek@poczta.pl>
2024-11-29 08:31:40 +0100

    Created oceny.js and begin work on it

---
+++ oceny/oceny.js
@@ -0,0 +1,11 @@
+class Student
+{
+    constructor(name,age,grades)
+    {
+        this.name = name
+        this.age = age
+        this.grades = []
+    }
+}
+
+const grades1 = new Student("Jurek", 17, [2,3,5,4,6])
