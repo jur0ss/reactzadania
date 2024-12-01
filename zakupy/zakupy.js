209cba4a61e066af680fa1602282ccd278c09542
jurekg <jurek.gryz2007@gmail.com>
2024-12-01 17:32:47 +0100

    Added add and remove methods

--- zakupy/zakupy.js
+++ zakupy/zakupy.js
@@ -5,4 +5,16 @@ class Cart
         this.products = []
     }
 
+    addProduct(name,price,amount)
+    {
+        const product = {name, price, amount}
+        this.products.push(product)
+    }
+
+    removeProduct(name)
+    {
+        const index = this.products.indexOf(name)
+        this.products = this.products.splice(index, 1)
+    }
+
 }
