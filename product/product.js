9b07a165e97e126c98db3b44f366c4f87e3b1c31
jurekg <jurek.gryz2007@gmail.com>
2024-12-01 17:21:01 +0100

    Added comments

--- product/product.js
+++ product/product.js
@@ -14,16 +14,15 @@ const p4 = new Product("tomato", 6, "vegetable")
 
 const products = [p1,p2,p3,p4]
 
-const fruits = products.filter(product => product.category === "fruit")
+const fruits = products.filter(product => product.category === "fruit") //filtrowanie produktu po kategorii
 const vegetables = products.filter(product => product.category === "vegetable")
 
-console.log(`Owoce: ${fruits.map(fruit => fruit.name).join(", ")}`);
-console.log(`Warzywa: ${vegetables.map(vegetable => vegetable.name).join(", ")}`);
-
+console.log(`Owoce: ${fruits.map(fruit => fruit.name).join(", ")}`) // wyświetla listę nazw owoców w koszyku, oddzielonych przecinkiem
+console.log(`Warzywa: ${vegetables.map(vegetable => vegetable.name).join(", ")}`)
 
 const min = 5
 const max = 10
-const priceRange = products.filter(product => {return product.price >= min && product.price <= max;});
+const priceRange = products.filter(product => {return product.price >= min && product.price <= max}) //zwraca produkty w danym przedziale cenowym
 
 console.log(`Produkty w tym zakresie cenowym: ${priceRange.map(product => product.name).join(", ")} `)
 
