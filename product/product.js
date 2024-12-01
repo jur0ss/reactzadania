e6d3e5593bfccf847a3a7bf81057be0e5f599d51
jurekg <jurek.gryz2007@gmail.com>
2024-12-01 16:03:34 +0100

    Displaying products using template strings and mapping

--- product/product.js
+++ product/product.js
@@ -10,9 +10,12 @@ class Product
 const p1 = new Product("apple",5,"fruit")
 const p2 = new Product("mango", 10, "fruit")
 const p3 = new Product("carrot", 4, "vegetable")
+const p4 = new Product("tomato", 6, "vegetable")
 
-const products = [p1,p2,p3]
+const products = [p1,p2,p3,p4]
 
 const fruits = products.filter(product => product.category === "fruit")
+const vegetables = products.filter(product => product.category === "vegetable")
 
-console.log(fruits)
+console.log(`Fruits: ${fruits.map(fruit => fruit.name).join(", ")}`);
+console.log(`Vegetables: ${vegetables.map(vegetable => vegetable.name).join(", ")}`);
