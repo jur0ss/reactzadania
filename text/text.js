3376bd0c91985adaf48c00e00cb2e6e8df13714d
jurekg <jurek.gryz2007@gmail.com>
2024-12-02 02:14:11 +0100

    Added remove special chars method

--- text/text.js
+++ text/text.js
@@ -1,4 +1,10 @@
 const processText = (
     text,
     { uppercase = false, reverse = false, removeSpecialChars = false } = {}
-  ) => {
+  ) => 
+    {
+        if (removeSpecialChars==true) 
+        {
+            text = text.replace(/[^a-zA-Z0-9\s]/g, '')
+        }
+    }
