fc0676324c1c2438fa0e03810cf4e53e917eaffd
jurekg <jurek.gryz2007@gmail.com>
2024-12-02 02:39:26 +0100

    Created zakupy.js and added constructor

--- zakupy/zakupy.js
+++ zakupy/zakupy.js
@@ -13,10 +13,6 @@ class Cart
 
     removeProduct(name)
     {
-        const index = this.products.indexOf(name)
-        if (index !== -1) { // Check if the product exists
-            this.products.splice(index, 1); // Remove the product
-        }
     }
 
 }
